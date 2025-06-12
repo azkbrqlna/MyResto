a1d6672b698b1134725348aa95df80905edbd96c
azkbrqlna <azkbrq@gmail.com>
2025-06-12 09:43:18 +0700

    add : card livewire page

--- app/Livewire/Pages/AllFoodPage.php
+++ app/Livewire/Pages/AllFoodPage.php
@@ -2,12 +2,33 @@
 
 namespace App\Livewire\Pages;
 
+use App\Models\Category;
+use App\Models\Foods;
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class AllFoodPage extends Component
 {
+    // use CategoryFilterTrait;
+
+    public $categories;
+    public $selectedCategories = [];
+    public $items;
+    public $title = "All Food";
+
+    public function mount(Foods $foods) {
+        $this->categories = Category::all();
+        $this->items = $foods->getAllFoods();
+    }
+
+    #[Layout('components.layouts.page')]
+
     public function render()
     {
-        return view('livewire.pages.all-food-page');
+        $filteredProducts = $this->getFilteredItems();
+        return view('product.all-food',[
+                'filteredProducts' => $filteredProducts
+        ]);
+        // return view('livewire.pages.all-food-page');
     }
 }
--- app/Livewire/Pages/CartPage.php
+++ app/Livewire/Pages/CartPage.php
@@ -3,11 +3,71 @@
 namespace App\Livewire\Pages;
 
 use Livewire\Component;
+use Livewire\Attributes\Session;
+use Livewire\Attributes\Layout;
 
 class CartPage extends Component
 {
+    // use CartManagement;
+
+    public $foods;
+    public $title = "All Foods";
+    public bool $selectAll = true;
+    public $selectedItems = [];
+
+    #[Session(key: 'cart_items')]
+    public $cartItems = [];
+    #[Session(key: 'has_unpaid_transaction')]
+    public $has_unpaid_transaction;
+
+    public function mount()
+    {
+        $this->updateSelectedItems();
+    }
+
+    public function updateSelectAll() {
+        foreach ($this->cartItems as $item) {
+            $item['selected'] = $this->selectAll;
+        }
+
+        $this->updateSelectedItems();
+    }
+
+    public function updateSelectedItems(){
+        $this->selectedItems = collect($this->cartItems)->filter(fn($item) => $item['selected'])->toArray();
+
+        $this->selectAll = count($this->selectedItems) === count($this->cartItems);
+
+        session(['has_unpaid_transaction' => false]);
+    }
+
+    public function deletedSelected(){
+        $this->cartItems = collect($this->cartItems)->filter(fn($item) => !$item['selected'])->toArray();
+
+        $selectedIds = collect($this->selectedItems)->map(fn($item) => $item['id'])->toArray();
+
+        $cartItemIds = collect(session('cart_items'), [])->map(fn($item) => $item['id'])->toArray();
+        $cartItemIds = array_diff($cartItemIds, $selectedIds);
+
+        session(['cart_items' => $cartItemIds]);
+
+        $this->selectedItems = [];
+    }   
+
+    public function checkout(){
+        if(empty($this->selectedItems)){
+            $this->addError('selectedItems','Please select at least one item');
+            return;
+        }
+
+        session(['cart_items' => $this->cartItems]);
+        
+        return $this->redirect('/checkout', navigate:true);
+    }
+
+    #[Layout('components.layouts.page')]
     public function render()
     {
-        return view('livewire.pages.cart-page');
+        return view('payment.cart');
     }
 }
