b4fa580f371de8247bf00af447d3e1f6d84f889b
azkbrqlna <azkbrq@gmail.com>
2025-06-12 10:54:17 +0700

    add : favorite page livewire page

--- app/Livewire/Pages/FavoritePage.php
+++ app/Livewire/Pages/FavoritePage.php
@@ -2,12 +2,33 @@
 
 namespace App\Livewire\Pages;
 
+use App\Models\Category;
+use App\Models\Foods;
 use Livewire\Component;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\On;
 
 class FavoritePage extends Component
 {
+    // use CategoryFilterTrait;
+
+    public $categories;
+    public $selectedCategories = [];
+    public $items;
+    public $title =  'Favorite';
+
+    public function mount(Foods $foods){
+        $this->categories = Category::all();
+        $this->items = $foods->getFavoriteFood();
+    }
+
+    #[Layout('components.layouts.page')]
     public function render()
     {
-        return view('livewire.pages.favorite-page');
+        $filteredProducts = $this->getFilteredItems();
+        // return view('livewire.pages.favorite-page');
+        return view('product.favorite',[
+            'filteredProducts' => $filteredProducts
+        ]);
     }
 }
