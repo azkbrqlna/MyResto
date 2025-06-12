7bf59f9a8cc849236d21448a60fa6392890c8b6e
azkbrqlna <azkbrq@gmail.com>
2025-06-12 10:06:23 +0700

    add : livewire page checkout page

--- app/Livewire/Pages/CheckoutPage.php
+++ app/Livewire/Pages/CheckoutPage.php
@@ -3,11 +3,51 @@
 namespace App\Livewire\Pages;
 
 use Livewire\Component;
+use App\Models\Foods;
+use Illuminate\Support\Facades\Session as FacadesSession;
+use Illuminate\Support\Str;
+use Livewire\Attributes\Session;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\On;
 
 class CheckoutPage extends Component
 {
+    // use CartManagement;
+    public $name;
+    public $phone;
+    #[Session(key:'table_number')]
+    public $tableNumber;
+    #[Session(key : 'tax')]
+    public $tax;
+    #[Session(key : 'has_unpaid_transaction')]
+    public $hasUnpaidTransaction;
+    #[Session(key : 'cart_items')]
+    public $cartItems = [];
+
+    public $title = "All Foods";
+
+    public $total;
+    public $subTotal;
+
+    public $paymentToken;
+
+    #[On('saved-user-info')]
+    public function mount(){
+        $this->name = session('name');
+        $this->phone = session('phone');
+        if (empty($this->cartItems)){
+            return redirect()->route('product.cart');
+        }
+
+        $this->paymentToken = Str::random(32);
+        session(['payment_token' => $this->paymentToken]);
+
+        $this->updateTotals();
+    }
+
+    #[Layout('components.layouts.app')]
     public function render()
     {
-        return view('livewire.pages.checkout-page');
+        return view('payment.checkout');
     }
 }
